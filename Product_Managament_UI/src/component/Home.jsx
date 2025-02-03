aca0097ea485cbf83f9ad1a45e1bb5765e7cc8f6
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-02-03 14:28:44 +0530

    final commit

--- Product_Managament_UI/src/component/Home.jsx
+++ Product_Managament_UI/src/component/Home.jsx
@@ -69,6 +69,8 @@ const Home = () => {
                         </tr>
                       })
                     }
+
+                    
                   </tbody>
                 </table>
               </div>
